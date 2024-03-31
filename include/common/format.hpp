d63e7086a4fcb3da755fa9a1cb9c871c72c2504d
Ian Kersz <kerszamaral@gmail.com>
2024-03-31 12:44:11 -0300

    Added const to to_underlying

--- include/common/format.hpp
+++ include/common/format.hpp
@@ -29,7 +29,7 @@ namespace fmt
     }
 
     template <typename E>
-    constexpr auto to_underlying(E e) noexcept
+    constexpr auto to_underlying(const E &e) noexcept
     {
         return static_cast<std::underlying_type_t<E>>(e);
     }
