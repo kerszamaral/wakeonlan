5b3b8e7bd95c2d7e3cf347726740f05c7b1a4785
kerszamaral <kerszamaral@gmail.com>
2024-04-13 01:43:58 -0300

    Fixed menu spacing

--- src/interface/output.cpp
+++ src/interface/output.cpp
@@ -3,6 +3,8 @@
 #include <thread>
 #include <iostream>
 #include <sstream>
+#include <iomanip>
+#include <cstring>
 
 #ifdef _WIN32
 constexpr auto CLEAR = "cls";
@@ -13,10 +15,37 @@ constexpr auto CLEAR = "clear";
 std::string make_pc_table(const pc_map_t &pc_map)
 {
     std::stringstream ss;
-    ss << "Hostname\tMAC Address\tIPv4 Address\tStatus\n";
+
+    constexpr const auto hostname_header = "Hostname";
+    constexpr const auto HOSTNAME_S_SIZE = 20;
+    ss << std::left;
+    ss << std::setw(HOSTNAME_S_SIZE) << hostname_header;
+
+    constexpr const auto mac_header = "MAC Address";
+    constexpr const auto MAC_S_SIZE = 20;
+    ss << std::setw(MAC_S_SIZE) << mac_header;
+
+    constexpr const auto ipv4_header = "IPv4 Address";
+    constexpr const auto IP_S_SIZE = 17;
+    ss << std::setw(IP_S_SIZE) << ipv4_header;
+
+    constexpr const auto status_header = "Status";
+    constexpr const auto STATUS_S_SIZE = 8;
+    ss << std::setw(STATUS_S_SIZE) << status_header << std::endl;
+
     for (auto &pc : pc_map)
     {
-        ss << pc.second.get_hostname() << "\t" << pc.second.get_mac().to_string() << "\t" << pc.second.get_ipv4().to_string() << "\t" << std::to_string(pc.second.get_status()) << "\n";
+        const auto &hostname = pc.second.get_hostname();
+        ss << std::setw(HOSTNAME_S_SIZE) << hostname.c_str();
+
+        const auto &mac = pc.second.get_mac().to_string();
+        ss << std::setw(MAC_S_SIZE) << mac.c_str();
+
+        const auto &ipv4 = pc.second.get_ipv4().to_string();
+        ss << std::setw(IP_S_SIZE) << ipv4.c_str();
+
+        const auto &status = pc.second.get_status() == PC_STATUS::AWAKE ? "Awake" : "Asleep";
+        ss << std::setw(STATUS_S_SIZE) << status << std::endl;
     }
     return ss.str();
 }
