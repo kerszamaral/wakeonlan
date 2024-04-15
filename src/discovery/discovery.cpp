98b5ccdfb4bf1a8157615e2e990567d285e24649
kerszamaral <kerszamaral@gmail.com>
2024-04-15 00:43:31 -0300

    Fixed forgotten cout

--- src/discovery/discovery.cpp
+++ src/discovery/discovery.cpp
@@ -31,8 +31,7 @@ void init_discovery(Threads::ProdCosum<PCInfo> &new_pcs, Threads::Signals &signa
     //? Our UDP connection side and packets
     UDPConn conn = UDPConn(disc_port_num);
     const auto hostname = PCInfo::getMachineName();
-    const auto mac = MacAddr::getMacAddr().value_or(MacAddr());
-    std::cout << "Mac: " << mac << std::endl;
+    const auto mac = MacAddr::getMacAddr().value();
     const auto data = std::make_pair(hostname, mac);
     Packet discovery_packet(PacketType::SSD, data);
     Packet discovery_ack_packet(PacketType::SSD_ACK, data);
