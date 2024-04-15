797c820980962174a91a668e937598307093802a
kerszamaral <kerszamaral@gmail.com>
2024-04-15 17:41:11 -0300

    Tried making ctrl+c handling work on windows

--- src/threads/shutdown.cpp
+++ src/threads/shutdown.cpp
@@ -2,14 +2,34 @@
 
 #include <csignal>
 #include "networking/sockets/socket.hpp"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 namespace Shutdown
 {
     std::function<void(int)> shutdown_handler;
+
+#ifdef _WIN32
+    BOOL WINAPI signal_handler(_In_ DWORD dwCtrlType)
+    {
+        switch (dwCtrlType)
+        {
+        case CTRL_C_EVENT:
+            shutdown_handler(dwCtrlType);
+            break;
+        default:
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+#else
     void signal_handler(int signal)
     {
         shutdown_handler(signal);
     }
+#endif
 
     void graceful_setup(Threads::Signals &signals)
     {
@@ -17,11 +37,17 @@ namespace Shutdown
         {
             signals.run.store(false);
         };
+
+#ifdef _WIN32
+        SetConsoleCtrlHandler(signal_handler, TRUE);
+#else
         std::signal(SIGINT, signal_handler);
+#endif
     }
 
     void graceful_shutdown()
     {
         Networking::Sockets::cleanup();
+        std::cout << "Shutting down..." << std::endl;
     }
 }
