81dd992266a4a049953fdec05daf39e5631835e1
kerszamaral <kerszamaral@gmail.com>
2024-04-16 11:22:27 -0300

    Fixed enter after ctrl+c exit

--- src/interface/input.cpp
+++ src/interface/input.cpp
@@ -5,7 +5,14 @@
 #include <functional>
 #include <string>
 #include <algorithm>
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <unistd.h>
+#include <sys/socket.h>
+#endif
 #include "common/format.hpp"
+#include "common/optional.hpp"
 
 typedef std::map<std::string_view, std::function<void(std::string_view)>> cmd_map_t;
 
@@ -48,21 +55,64 @@ void run_cmd(const cmd_map_t &cmd_map, std::string_view cmd, std::string_view ar
     }
 }
 
+// https://stackoverflow.com/questions/65359021/how-to-define-a-non-blocking-input-in-c
+bool stdinHasData()
+{
+#ifdef _WIN32
+    // this works by harnessing Windows' black magic:
+    return _kbhit();
+#else
+    // using a timeout of 0 so we aren't waiting:
+    struct timespec timeout
+    {
+        0l, 0l
+    };
+
+    // create a file descriptor set
+    fd_set fds{};
+
+    // initialize the fd_set to 0
+    FD_ZERO(&fds);
+    // set the fd_set to target file descriptor 0 (STDIN)
+    FD_SET(0, &fds);
+
+    // pselect the number of file descriptors that are ready, since
+    //  we're only passing in 1 file descriptor, it will return either
+    //  a 0 if STDIN isn't ready, or a 1 if it is.
+    return pselect(0 + 1, &fds, nullptr, nullptr, &timeout, nullptr) == 1;
+#endif
+}
+
+opt::optional<std::string> async_getline()
+{
+    if (!stdinHasData())
+    {
+        return opt::nullopt;
+    }
+
+    std::string buffer;
+    std::getline(std::cin, buffer);
+    if (buffer.empty())
+    {
+        return opt::nullopt;
+    }
+    return buffer;
+}
+
 void ReadCin(Threads::Signals &signals, Threads::ProdCosum<hostname_t> &wakeups)
 {
-    std::string buffer;
     auto cmd_map = create_cmds(signals.run, wakeups);
 
     while (signals.run.load())
     {
-        std::getline(std::cin, buffer);
+        auto buffer = async_getline();
 
-        if (buffer.empty())
+        if (!buffer.has_value())
         {
             continue;
         }
 
-        auto cmd_args = fmt::split(fmt::to_lower(buffer), ' ');
+        auto cmd_args = fmt::split(fmt::to_lower(buffer.value()), ' ');
         std::string_view cmd = cmd_args[0];
         std::string_view args = cmd_args.size() > 1 ? cmd_args[1] : "";
 
