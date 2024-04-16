d6943bdf2908b41e20be2053a12bbcedf7181b10
kerszamaral <kerszamaral@gmail.com>
2024-04-16 14:32:38 -0300

    better manager displaying

--- src/interface/output.cpp
+++ src/interface/output.cpp
@@ -8,36 +8,32 @@
 #include "common/platform.hpp"
 #include "common/pcinfo.hpp"
 
-#ifdef OS_WIN
-constexpr auto CLEAR = "cls";
-#else
-constexpr auto CLEAR = "clear";
-#endif
-
-std::string make_pc_table(const pc_map_t &pc_map)
+std::string make_pc_table(const pc_map_t &pc_map, const std::atomic<bool> &is_manager)
 {
     std::stringstream ss;
+    constexpr const auto MANAGER_TAG = " *";
 
-    constexpr const auto hostname_header = "Hostname";
+    constexpr const auto HOSTNAME_HEADER = "Hostname";
     constexpr const auto HOSTNAME_S_SIZE = 20;
+    const auto hostname_header = std::string(HOSTNAME_HEADER) + (is_manager.load() ? MANAGER_TAG : "");
     ss << std::left;
-    ss << std::setw(HOSTNAME_S_SIZE) << hostname_header;
+    ss << std::setw(HOSTNAME_S_SIZE) << hostname_header.c_str();
 
-    constexpr const auto mac_header = "MAC Address";
+    constexpr const auto MAC_HEADER = "MAC Address";
     constexpr const auto MAC_S_SIZE = 20;
-    ss << std::setw(MAC_S_SIZE) << mac_header;
+    ss << std::setw(MAC_S_SIZE) << MAC_HEADER;
 
-    constexpr const auto ipv4_header = "IPv4 Address";
+    constexpr const auto IPV4_HEADER = "IPv4 Address";
     constexpr const auto IP_S_SIZE = 17;
-    ss << std::setw(IP_S_SIZE) << ipv4_header;
+    ss << std::setw(IP_S_SIZE) << IPV4_HEADER;
 
-    constexpr const auto status_header = "Status";
+    constexpr const auto STATUS_HEADER = "Status";
     constexpr const auto STATUS_S_SIZE = 8;
-    ss << std::setw(STATUS_S_SIZE) << status_header << std::endl;
+    ss << std::setw(STATUS_S_SIZE) << STATUS_HEADER << std::endl;
 
     for (auto &pc : pc_map)
     {
-        const auto &hostname = pc.second.get_hostname();
+        const auto hostname = pc.second.get_hostname() + (pc.second.get_is_manager() ? MANAGER_TAG : "");
         ss << std::setw(HOSTNAME_S_SIZE) << hostname.c_str();
 
         const auto &mac = pc.second.get_mac().to_string();
@@ -52,10 +48,16 @@ std::string make_pc_table(const pc_map_t &pc_map)
     return ss.str();
 }
 
+#ifdef OS_WIN
+constexpr auto CLEAR = "cls";
+#else
+constexpr auto CLEAR = "clear";
+#endif
+
 void WriteCout(Threads::Atomic<pc_map_t> &pc_map, Threads::Signals &signals)
 {
     constexpr const auto CHECK_DELAY = std::chrono::milliseconds(100);
-    std::string table = pc_map.compute(make_pc_table);
+    std::string table = pc_map.compute(make_pc_table, signals.is_manager);
     while (signals.run.load())
     {
 #ifndef DEBUG
@@ -67,7 +69,7 @@ void WriteCout(Threads::Atomic<pc_map_t> &pc_map, Threads::Signals &signals)
         {
             std::this_thread::sleep_for(CHECK_DELAY);
         }
-        table = pc_map.compute(make_pc_table);
+        table = pc_map.compute(make_pc_table, signals.is_manager);
         signals.update.store(false);
     }
 }
