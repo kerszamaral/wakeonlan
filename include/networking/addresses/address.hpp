26639f062965bd34c7846c026adeb5f7ef6f7bbb
kerszamaral <kerszamaral@gmail.com>
2024-04-17 02:10:20 -0300

    Added constexpr to address

--- include/networking/addresses/address.hpp
+++ include/networking/addresses/address.hpp
@@ -2,6 +2,7 @@
 
 #include "common/platform.hpp"
 
+#include "common/format.hpp"
 #include "networking/addresses/ipv4.hpp"
 #include "networking/addresses/port.hpp"
 
@@ -15,8 +16,23 @@ namespace Networking::Addresses
         IPv6 = AF_INET6
     };
 
-    void set_saddr(addr_t &addr, const uint32_t &new_addr);
-    uint32_t get_saddr(const addr_t &addr);
+    constexpr void set_saddr(addr_t &addr, const uint32_t &new_addr) noexcept
+    {
+#ifdef OS_WIN
+        addr.sin_addr.S_un.S_addr = new_addr;
+#else
+        addr.sin_addr.s_addr = new_addr;
+#endif
+    }
+
+    constexpr uint32_t get_saddr(const addr_t &addr) noexcept
+    {
+#ifdef OS_WIN
+        return addr.sin_addr.S_un.S_addr;
+#else
+        return addr.sin_addr.s_addr;
+#endif
+    }
 
     class Address
     {
@@ -25,33 +41,69 @@ namespace Networking::Addresses
         Port port;
         addr_t addr;
 
-        void init_addr();
+        constexpr void init_addr() noexcept
+        {
+            addr.sin_family = fmt::to_underlying(IPVersion::IPv4);
+            addr.sin_port = port.getPort();
+            set_saddr(addr, ip.to_network_order());
+        }
 
     public:
-        Address() : ip(), port() { init_addr(); }
-        Address(const std::string &address);
-        Address(const IPv4 &ip, const Port &port) : ip(ip), port(port) { init_addr(); }
-        Address(const std::string &ip, const Port &port) : ip(ip), port(port) { init_addr(); }
-        Address(const IPv4 &ip, const uint16_t &port) : ip(ip), port(port) { init_addr(); }
-        Address(const std::string &ip, const uint16_t &port) : ip(ip), port(port) { init_addr(); }
-        Address(const addr_t &address) : ip(get_saddr(address)), port(address.sin_port), addr(address) {}
-        Address(const Address &address) : ip(address.ip), port(address.port), addr(address.addr) {}
+        constexpr Address() noexcept : ip(), port() { init_addr(); }
+        constexpr Address(const std::string &address)
+        {
+            auto colon = address.find(':');
+            if (colon == std::string::npos)
+            {
+                throw std::runtime_error("Invalid address");
+            }
+            ip = IPv4(address.substr(0, colon));
+            port = Port(fmt::stoi(address.substr(colon + 1)));
+            init_addr();
+        }
+        constexpr Address(const IPv4 &ip, const Port &port) noexcept : ip(ip), port(port) { init_addr(); }
+        constexpr Address(const std::string &ip, const Port &port) : ip(ip), port(port) { init_addr(); }
+        constexpr Address(const IPv4 &ip, const uint16_t &port) noexcept : ip(ip), port(port) { init_addr(); }
+        constexpr Address(const std::string &ip, const uint16_t &port) : ip(ip), port(port) { init_addr(); }
+        constexpr Address(const addr_t &address) noexcept : ip(get_saddr(address)), port(address.sin_port), addr(address) {}
 
-        ~Address() {}
+        constexpr void setIP(const IPv4 &new_ip) noexcept
+        {
+            ip = new_ip;
+            set_saddr(addr, ip.to_network_order());
+        }
+        constexpr void setIP(const std::string &new_ip) { setIP(IPv4(new_ip)); }
+        constexpr void setPort(const Port &new_port) noexcept
+        {
+            port = new_port;
+            addr.sin_port = port.getPort();
+        }
 
-        void setIP(const IPv4 &new_ip);
-        void setIP(const std::string &new_ip) { setIP(IPv4(new_ip)); }
-        void setPort(const Port &new_port);
-        void setPort(const uint16_t &new_port) { setPort(Port(new_port)); }
-        void setAddr(const addr_t &new_addr);
+        constexpr void setPort(const uint16_t &new_port) noexcept { setPort(Port(new_port)); }
+        constexpr void setAddr(const addr_t &new_addr) noexcept
+        {
+            addr = new_addr;
+            ip = IPv4(get_saddr(addr));
+            port = Port(addr.sin_port);
+        }
 
-        IPv4 getIp() const { return ip; }
-        Port getPort() const { return port; }
-        addr_t getAddr() const { return addr; }
+        constexpr IPv4 getIp() const noexcept { return ip; }
+        constexpr Port getPort() const noexcept { return port; }
+        constexpr addr_t getAddr() const noexcept { return addr; }
 
-        std::string to_string() const;
+        std::string to_string() const
+        {
+            return ip.to_string() + ":" + port.to_string();
+        }
 
-        friend std::ostream &operator<<(std::ostream &os, const Address &a);
-        bool operator==(const Address &other) const;
+        friend std::ostream &operator<<(std::ostream &os, const Address &a)
+        {
+            os << a.to_string();
+            return os;
+        }
+        constexpr bool operator==(const Address &other) const noexcept
+        {
+            return ip == other.ip && port == other.port;
+        }
     };
 }
