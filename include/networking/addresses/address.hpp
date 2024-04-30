8d52b8664b0f8463840787e92fa36e1c703b02b1
Ian Kersz <kerszamaral@gmail.com>
2024-04-30 13:49:32 -0300

    Removed more constexpr

--- include/networking/addresses/address.hpp
+++ include/networking/addresses/address.hpp
@@ -23,7 +23,7 @@ namespace Networking::Addresses
         Port port;
         addr_t addr;
 
-        constexpr void init_addr() noexcept
+        void init_addr() noexcept
         {
             addr.sin_family = fmt::to_underlying(IPVersion::IPv4);
             port.setAddrPort(addr);
@@ -31,8 +31,8 @@ namespace Networking::Addresses
         }
 
     public:
-        constexpr Address() noexcept : ip(), port() { init_addr(); }
-        constexpr Address(const std::string &address)
+        Address() noexcept : ip(), port() { init_addr(); }
+        Address(const std::string &address)
         {
             auto colon = address.find(':');
             if (colon == std::string::npos)
@@ -43,14 +43,14 @@ namespace Networking::Addresses
             port = Port(fmt::stoi(address.substr(colon + 1)));
             init_addr();
         }
-        constexpr Address(const IPv4 &ip, const Port &port) noexcept : ip(ip), port(port) { init_addr(); }
-        constexpr Address(const std::string &ip, const Port &port) : ip(ip), port(port) { init_addr(); }
-        constexpr Address(const IPv4 &ip, const port_t &port) noexcept : ip(ip), port(port) { init_addr(); }
-        constexpr Address(const std::string &ip, const port_t &port) : ip(ip), port(port) { init_addr(); }
-        constexpr Address(const addr_t &address) noexcept : ip(address), port(address), addr(address) {}
-        constexpr Address(const Port &port) noexcept : ip(), port(port) { init_addr(); }
-        constexpr Address(const port_t &port) noexcept : ip(), port(port) { init_addr(); }
-        constexpr Address(const IPv4 &ip) noexcept : ip(ip), port() { init_addr(); }
+        Address(const IPv4 &ip, const Port &port) noexcept : ip(ip), port(port) { init_addr(); }
+        Address(const std::string &ip, const Port &port) : ip(ip), port(port) { init_addr(); }
+        Address(const IPv4 &ip, const port_t &port) noexcept : ip(ip), port(port) { init_addr(); }
+        Address(const std::string &ip, const port_t &port) : ip(ip), port(port) { init_addr(); }
+        Address(const addr_t &address) noexcept : ip(address), port(address), addr(address) {}
+        Address(const Port &port) noexcept : ip(), port(port) { init_addr(); }
+        Address(const port_t &port) noexcept : ip(), port(port) { init_addr(); }
+        Address(const IPv4 &ip) noexcept : ip(ip), port() { init_addr(); }
 
         constexpr void setIP(const IPv4 &new_ip) noexcept
         {
@@ -58,14 +58,14 @@ namespace Networking::Addresses
             ip.to_addr(addr);
         }
         constexpr void setIP(const std::string &new_ip) { setIP(IPv4(new_ip)); }
-        constexpr void setPort(const Port &new_port) noexcept
+        void setPort(const Port &new_port) noexcept
         {
             port = new_port;
             port.setAddrPort(addr);
         }
 
-        constexpr void setPort(const port_t &new_port) noexcept { setPort(Port(new_port)); }
-        constexpr void setAddr(const addr_t &new_addr) noexcept
+        void setPort(const port_t &new_port) noexcept { setPort(Port(new_port)); }
+        void setAddr(const addr_t &new_addr) noexcept
         {
             addr = new_addr;
             ip = IPv4(addr);
