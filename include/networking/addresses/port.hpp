45dab18147ed8a153fcae8e711be2766b2329933
Ian Kersz <kerszamaral@gmail.com>
2024-04-30 13:41:54 -0300

    removed constexpr from non constexpr functions

--- include/networking/addresses/port.hpp
+++ include/networking/addresses/port.hpp
@@ -21,14 +21,14 @@ namespace Networking::Addresses
     public:
         constexpr Port() noexcept : Port(0) {}
         constexpr Port(const uint16_t &port) noexcept : m_port(port) {}
-        constexpr Port(const sockaddr_in &addr) noexcept : m_port(from_network_order(addr.sin_port)) {}
+        Port(const sockaddr_in &addr) noexcept : m_port(from_network_order(addr.sin_port)) {}
 
         constexpr port_t getPort() const noexcept
         {
             return m_port;
         }
 
-        constexpr port_t to_network_order() const noexcept
+        port_t to_network_order() const noexcept
         {
             return to_network_order(m_port);
         }
@@ -38,7 +38,7 @@ namespace Networking::Addresses
             m_port = port;
         }
 
-        constexpr void setAddrPort(sockaddr_in &addr) const noexcept
+        void setAddrPort(sockaddr_in &addr) const noexcept
         {
             addr.sin_port = this->to_network_order();
         }
@@ -54,12 +54,12 @@ namespace Networking::Addresses
             return os;
         }
 
-        constexpr static port_t from_network_order(const port_t &port) noexcept
+        static port_t from_network_order(const port_t &port) noexcept
         {
             return ::ntohs(port);
         }
 
-        constexpr static port_t to_network_order(const port_t &port) noexcept
+        static port_t to_network_order(const port_t &port) noexcept
         {
             return ::htons(port);
         }
